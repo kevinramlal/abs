74a446d91dd429dc41717beb41a0857a885a5ab2
Kevin Ramlal <kevinramlal58@gmail.com>
2019-08-21 13:28:14 -0700

    Added dates file, and implement blacks formula

--- A1/a1.py
+++ A1/a1.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn as sk
 import datetime
+from scipy.stats import norm
 #Helper Functions
 def get_days_act_360(start_date,end_date):
     return (end_date-start_date).days/360
@@ -37,10 +38,14 @@ master_rates['Zero'] = zr_temp
 atm_cap = pd.read_csv('atm_cap.csv', header = None) #extracted from 20040830_usd23_atm_caps file
 atm_cap.columns = ['Maturity', 'Black Imp Vol']
 
-#Import date column and format 
-dates = pd.read_csv('dates.csv',header = None, dtype = str) #from Bloomberg files USD23 tab
-dates = pd.to_datetime(dates[0])
+#Import Expiry, Settlement Date column and format as datetime
+dates_all = pd.read_csv('dates_expiry_settle.csv',dtype = str) #from Bloomberg files USD23 tab
+dates = pd.to_datetime(dates_all['Settlement Date']) #settlement dates
 dates.columns = ['Date']
+
+dates_settle = pd.to_datetime(dates_all['Expiry Date'])
+dates_settle.columns = ['Settle_Date']
+
 
 #-----------------------------------------------------------------
 #a) Caclulate Discount factors Z(D_i)
@@ -51,6 +56,8 @@ start_date = dates[0]
 master_rates['Dates'] =np.array(dates) #We don't need 2004-09-01 for zeros
 master_rates['T_30_360'] = np.array(dates.apply(lambda x: get_days_30I_360(start_date,x)))
 master_rates['Discount'] = 1/(1+(master_rates['Zero']/100)/2)**(2*master_rates['T_30_360'])
+master_rates['Expiry Dates'] = np.array(dates_settle)
+master_rates['Expiry_day_count'] = np.array(dates_settle.apply(lambda x: (x - master_rates['Dates'][0]).days))
 
 print(master_rates)
 print("a) Discount Factors \n", master_rates[['Dates','Zero','Discount']].head(25), "\n")
@@ -68,6 +75,7 @@ master_rates['Tau'] = master_rates['T_ACT_360'].diff(1)
 #Forward Rates 
 forwards = np.array((1/master_rates['Tau'])*\
 						((-master_rates['Discount'].diff(1))/master_rates['Discount']))
+
 master_rates['Forward'] = forwards
 
 print("b) Forward Rates \n", master_rates[['Dates','Discount','Forward']].head(25), "\n")
@@ -105,23 +113,68 @@ print("c) ATM Strike Rates vs Maturity \n", cap_master_df, "\n")
 #use the same flat vol for all quarterly caplets when calculating a specific cap
 
 
+
+#---------Implementing Blacks Formula
 flat_vols = np.array(atm_cap['Black Imp Vol'])
 print(flat_vols)
 
 forward_libor = np.array(fwds['Fwds'])
-print(forward_libor,len(forward_libor)) #should be 30
+print(forward_libor,len(forward_libor))
+master_rates['Forward_Libor'] = forward_libor #Has one extra entry at end that we cannot compute? 
 
 cap_master_df['Flat_Vol'] = flat_vols
 
 def d_1_2(flat_vol,forward_libor,t,strike,type = 1):
-	d1 = (1/flat_vol*np.sqrt(t))*np.log(forward/strike) + 0.5*flat_vol*np.sqrt(t)
+	d1 = (1/(flat_vol*np.sqrt(t)))*np.log(forward_libor/strike) + 0.5*flat_vol*np.sqrt(t)
 	if type == 1:
 		return d1
 	elif type == 2:
 		return d1 - flat_vol*np.sqrt(t)
 
-# def caplet(master_rates,t_i,)
+#index by np.arange(1,maturity,4)
+
+def caplet(master_rates,time_index,N, flat_vol, strike):
+    """
+    master_rates - DataFrame that contains dates, discount rates, and forward rates 
+    from above.
+    time_index - index based on T_30_360 convenion (example - 0.75 -> 3)
+    """
+    t_i = master_rates['Expiry_day_count'][time_index]/365
+    tau = master_rates['Tau'][time_index +1] 
+    fwd = master_rates['Forward'][time_index+1]
+    fv = flat_vol/100
+    discount = master_rates['Discount'][time_index+1]
+    d1 = d_1_2(fv,fwd,t_i,strike)
+    d2 = d_1_2(fv,fwd,t_i,strike,type =2)
+    phi_d1 = norm.cdf(d1)
+    phi_d2 = norm.cdf(d2)
+    # print(phi_d1,phi_d1,discount,tau,N)
+    # print((N*tau)*discount*(fwd*phi_d1 - strike*phi_d2))
+    pv = ((N*tau)*discount*(fwd*phi_d1 - strike*phi_d2))
+
+    return pv
+
+###TEST CASE: MATCHES WITH EXCEL SHEET###
+print("-------Test Case: 1 Year Cap--------\n")
+maturity = cap_master_df['Maturity'][0]
+flat_vol = cap_master_df['Flat_Vol'][0]
+strike = cap_master_df['ATM Strike'][0]
+caplet_range = np.arange(1,maturity*4)
+# print(caplet_range)
+caplet_pv = []
+for index in caplet_range:
+    caplet_pv.append(caplet(master_rates,index,10000000,flat_vol,strike))
+print("------Prices of Caplets-------\n",caplet_pv,'\n')
+print("1 Year Cap Value : ", round(sum(caplet_pv),2))
 
 
 if __name__ == '__main__':
-		print(master_rates[['T_30_360','T_ACT_360']])
+    #testing caplet
+    maturity = cap_master_df['Maturity'][0]
+    flat_vol = cap_master_df['Flat_Vol'][0]
+    strike = cap_master_df['ATM Strike'][0]
+    caplet_range = np.arange(1,maturity*4)
+    caplet_pv = []
+    for index in caplet_range:
+        caplet_pv.append(caplet(master_rates,index,10000000,flat_vol,strike))
+    # print(caplet_pv)
